338cb780e4f1e72379ed585ca2bea77e38ed6599
RABijl <r.a.bijl@hotmail.nl>
2021-04-23 08:26:38 +0000

    adds calibrated e steps

--- firmware/V1.2/Marlin-2.0.7.2-SKR-mini-E3-V1.2/Marlin/Configuration.h
+++ firmware/V1.2/Marlin-2.0.7.2-SKR-mini-E3-V1.2/Marlin/Configuration.h
@@ -743,7 +743,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 } // TODO: calibrate 
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 110.92 } // TODO: calibrate 
 
 /**
  * Default Max Feed Rate (mm/s)
